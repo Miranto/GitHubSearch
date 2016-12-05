8504042be7d6e6cd3d9f873d8d7c868d229132d4
Mirczenzo <mateusz.mirkowski@visentcoders.com>
2016-12-05 09:40:24 +0100

    Initial Commit

---
+++ GitHubSearch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitHubSearch
+//
+//  Created by grzesiek on 05/12/2016.
+//  Copyright © 2016 Mateusz Mirkowski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
